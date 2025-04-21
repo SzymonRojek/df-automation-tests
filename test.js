5fade962e8aeb655ae5e593d9a5122395f896d76
szymon rojek <szymonrojek@szymons-MacBook-Pro.local>
2025-04-21 21:48:46 +0100

    removed test.js file

--- test.js
+++
@@ -1,9 +0,0 @@
-async function getErrorClass() {
-    await t
-    .navigateTo('https://account.directferries.com/?culture=en-GB')
-    .typeText(emailInput, 'amazing')
-    .typeText(bookingRefInput, 'bookingReference')
-    //.click(loginButton);
-};
-
-await getErrorClass();
